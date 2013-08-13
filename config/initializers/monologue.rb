a1eb516c73f64215e7b582437d76f8b698d5e41e
Jean-Philippe Boily <j@jipi.ca>
2013-08-12 22:16:51 -0400

    add monologue and monologue-markdown

---
+++ config/initializers/monologue.rb
@@ -0,0 +1,13 @@
+Monologue.site_name            = "Demo site"
+Monologue.site_subtitle        = "Some subtitle or whatever"
+Monologue.site_url             = "http://jipiboily.com"
+
+Monologue.admin_force_ssl       = false
+Monologue.posts_per_page        = 5
+
+# LOCALE
+Monologue.twitter_locale       = "en" # "fr"
+Monologue.facebook_like_locale = "en_US" # "fr_CA"
+Monologue.google_plusone_locale = "en"
+
+Monologue.sidebar = ["latest_posts"]
