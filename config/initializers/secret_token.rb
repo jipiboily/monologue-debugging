336f8ffb1d4736fbcf8a0e962885332710d05032
Jean-Philippe Boily <j@jipi.ca>
2013-08-12 22:03:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonologueMarkdownContentPipeLineIssueDemo::Application.config.secret_token = '58297e91fc2e862165622b2c7646c1a00e3bdd9b4fa181f6cee3882b64630a5f8cef0db928c1b5077092be9c09e4c5830b9c7f7245635b23ebf717959c1b3b7b'
